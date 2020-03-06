3150dd8eee3660b45483631d30e891810dd96491
Ricardo Pereira <rikardupereira@gmail.com>
2020-03-06 13:58:27 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/ricardopereira/cursomc/resources/PedidoResource.java
+++ src/main/java/com/ricardopereira/cursomc/resources/PedidoResource.java
@@ -20,9 +20,9 @@ public class PedidoResource {
 	
 	
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)	
-	public ResponseEntity<?> buscar(@PathVariable Integer id) {		
+	public ResponseEntity<Pedido> buscar(@PathVariable Integer id) {		
 		
-		Pedido obj = service.buscar(id);
+		Pedido obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
 	}
