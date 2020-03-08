2d5398f586b735a31afb6dbbdf256afec9814eef
Ricardo Pereira <rikardupereira@gmail.com>
2020-03-08 12:13:36 -0300

    Validacao customizada: email nao repetido na atualização de Cliente

--- src/main/java/com/ricardopereira/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/ricardopereira/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.ricardopereira.cursomc.domain.Cliente;
+import com.ricardopereira.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{	
 	private static final long serialVersionUID = 1L;
 	
@@ -18,6 +20,7 @@ public class ClienteDTO implements Serializable{
 	@NotEmpty(message="Preenchimento obrigatório")
 	@Length(min=5, max=120, message="O tamanho deve ser entre 5 a 120 caracteres")
 	private String nome;
+	
 	
 	@NotEmpty(message="Preenchimento obrigatório")
 	@Email(message="email inválido")
