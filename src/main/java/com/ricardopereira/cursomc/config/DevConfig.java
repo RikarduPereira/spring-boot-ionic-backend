f83b54c1619d3c78b916c99902fefb3f5e94ce4a
Ricardo Pereira <rikardupereira@gmail.com>
2020-03-16 11:35:34 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/ricardopereira/cursomc/config/DevConfig.java
+++ src/main/java/com/ricardopereira/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.ricardopereira.cursomc.services.DBService;
+import com.ricardopereira.cursomc.services.EmailService;
+import com.ricardopereira.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -34,6 +36,9 @@ public class DevConfig {
 		return true;
 	}
 	
-	
+	@Bean 
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
 	
 }
